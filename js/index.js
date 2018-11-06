ba05dd9801f33d484288ee517143f130646c2fba
sinchang <sinchangwen@gmail.com>
2018-11-06 23:43:17 +0800

    Update ScrollReveal

--- js/index.js
+++ js/index.js
@@ -2,65 +2,63 @@ $(function() {
   window.shake(function() {
     $('#index').hide()
     $('#container').show()
+    run()
   })
 
-  var options = {
-    reset: true,
-    mobile: true
+  function run() {
+    sr.reveal('.p2-img', {
+      distance: '100px',
+      origin: 'left'
+    })
+
+    sr.reveal('.p3-img', {
+      distance: '100px',
+      origin: 'left'
+    })
+
+    sr.reveal('.p2-text', {
+      distance: '100px',
+      origin: 'right'
+    })
+
+    sr.reveal('.p4-text', {
+      distance: '100px',
+      origin: 'left'
+    })
+
+    sr.reveal('.p4-img', {
+      distance: '100px',
+      origin: 'right'
+    })
+
+    sr.reveal('.p5-img', {
+      distance: '100px',
+      origin: 'left'
+    })
+
+    sr.reveal('.p5-text', {
+      distance: '100px',
+      origin: 'right'
+    })
+
+    sr.reveal('.dot-1', {
+      distance: '1000px',
+      origin: 'top'
+    })
+
+    sr.reveal('.dot-2', {
+      distance: '1000px',
+      origin: 'top'
+    })
+
+    sr.reveal('.dot-3', {
+      distance: '1000px',
+      origin: 'top'
+    })
+
+    sr.reveal('.dot-4', {
+      distance: '1000px',
+      origin: 'top'
+    })
   }
-
-  ScrollReveal(options).reveal('.p2-img', {
-    distance: '100px',
-    origin: 'left'
-  })
-
-  ScrollReveal(options).reveal('.p3-img', {
-    distance: '100px',
-    origin: 'left'
-  })
-
-  ScrollReveal(options).reveal('.p2-text', {
-    distance: '100px',
-    origin: 'right'
-  })
-
-  ScrollReveal(options).reveal('.p4-text', {
-    distance: '100px',
-    origin: 'left'
-  })
-
-  ScrollReveal(options).reveal('.p4-img', {
-    distance: '100px',
-    origin: 'right'
-  })
-
-  ScrollReveal(options).reveal('.p5-img', {
-    distance: '100px',
-    origin: 'left'
-  })
-
-  ScrollReveal(options).reveal('.p5-text', {
-    distance: '100px',
-    origin: 'right'
-  })
-
-  ScrollReveal(options).reveal('.dot-1', {
-    distance: '1000px',
-    origin: 'top'
-  })
-
-  ScrollReveal(options).reveal('.dot-2', {
-    distance: '1000px',
-    origin: 'top'
-  })
-
-  ScrollReveal(options).reveal('.dot-3', {
-    distance: '1000px',
-    origin: 'top'
-  })
-
-  ScrollReveal(options).reveal('.dot-4', {
-    distance: '1000px',
-    origin: 'top'
-  })
 })
