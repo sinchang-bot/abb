88e3c415e17f959ebddf295e8ed8e93b02700d59
sinchang <sinchangwen@gmail.com>
2018-11-09 17:49:39 +0800

    Add progress

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(function() {
   var shaked = false
+  var running = false
   window.shake(function() {
     if (shaked) return
     shaked = true
@@ -8,8 +9,41 @@ $(function() {
     setTimeout(function() {
       $('#p-wrap').show()
       run()
-      $.scrollTo($('body').height() - window.innerHeight, 8000, 'linear')
-    }, 8 * 1000);
+      $.scrollTo($('body').height() - window.innerHeight, 8000, {
+        easing: 'linear',
+        onAfter: function() {},
+        step: function(v) {
+          if (
+            !running &&
+            v >
+              $('body').height() -
+                window.innerHeight -
+                $('.p-con-3').height() / 2
+          ) {
+            running = true
+            $('.gif')
+              .show()
+              .attr(
+                'src',
+                './images/1.gif?nocache=' + Math.floor(Math.random() * 1000)
+              )
+            $('#percent').show()
+            var n = 1
+            var t = setInterval(function() {
+              var count = n * 10
+              if (n === 10) {
+                count = 99
+                $('#percent').text(count + '%')
+                clearInterval(t)
+              } else {
+                $('#percent').text(count + '%')
+              }
+              n++
+            }, 200)
+          }
+        }
+      })
+    }, 8 * 1000)
   })
 
   function run() {
@@ -26,7 +60,7 @@ $(function() {
     sr.reveal('.p2-text', {
       distance: '100px',
       origin: 'right',
-      delay:600
+      delay: 600
     })
 
     sr.reveal('.p4-text', {
